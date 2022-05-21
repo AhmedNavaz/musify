4cc02ee7c76b095ef6e24d72fc5219f5fe518e77
Ahmed Nawaz <ahmednawaz.dev@gmail.com>
2022-05-21 19:29:06 +0500

    leve

--- lib/meta/views/bottom_navigation/add/add_playlist.dart
+++ lib/meta/views/bottom_navigation/add/add_playlist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musify/app/constants/assets.constant.dart';
 
 class AddPlaylist extends StatefulWidget {
   const AddPlaylist({Key? key}) : super(key: key);
@@ -10,70 +11,69 @@ class AddPlaylist extends StatefulWidget {
 class _AddPlaylistState extends State<AddPlaylist> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black12,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: Text(
-            "Add Playlist",
-            style: TextStyle(
-                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-          ),
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: true,
+        title: Text(
+          "Add Playlist",
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
         ),
-        body: SingleChildScrollView(
-          child: Column(children: [
-            InkWell(
-              child: Image.asset("./assets/uploadimage.jpg"),
-              onTap: () => print("Upload Function Called."),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  labelText: "Playlist Name",
-                  labelStyle: TextStyle(color: Colors.amber),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              minLines: 2,
-              maxLines: 5,
-              keyboardType: TextInputType.multiline,
-              decoration: InputDecoration(
-                labelText: "Description",
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          InkWell(
+            child: Image.asset(Assets.splashLogo),
+            onTap: () => print("Upload Function Called."),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Playlist Name",
                 labelStyle: TextStyle(color: Colors.amber),
                 filled: true,
                 fillColor: Colors.white,
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
+                    borderRadius: BorderRadius.circular(20.0))),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            minLines: 2,
+            maxLines: 5,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              labelText: "Description",
+              labelStyle: TextStyle(color: Colors.amber),
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
               ),
             ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Add Playlist"),
-                  style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 25.0, vertical: 15.0),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0)),
-                      primary: Colors.amber,
-                      onPrimary: Colors.white),
-                )
-              ],
-            ),
-          ]),
-        ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Add Playlist"),
+                style: ElevatedButton.styleFrom(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0)),
+                    primary: Colors.amber,
+                    onPrimary: Colors.white),
+              )
+            ],
+          ),
+        ]),
       ),
     );
   }
--- lib/meta/views/bottom_navigation/add/body.dart
+++ lib/meta/views/bottom_navigation/add/body.dart
@@ -1,15 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:musify/app/constants/assets.constant.dart';
+import 'package:musify/app/constants/controller.constant.dart';
+import 'package:musify/core/router/router_generator.dart';
+import 'package:musify/meta/utils/app_theme.dart';
 
 class AddView extends StatelessWidget {
-  const AddView({Key? key}) : super(key: key);
+  AddView({Key? key}) : super(key: key);
+
+  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text('Add'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Skip",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(fontSize: 18, color: AppTheme.primaryColor),
+                  ),
+                  Text(
+                    "Select Playlist",
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      navigationController
+                          .navigateToNamed(RouteGenerator.uploadMusic);
+                    },
+                    child: Text(
+                      "Next",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(fontSize: 18, color: AppTheme.primaryColor),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 0,
+                      mainAxisSpacing: 10),
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5),
+                                child: Image.asset(
+                                  Assets.splashLogo,
+                                  width: 90,
+                                  height: 90,
+                                ),
+                              ),
+                              Text(
+                                "Playlist",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(color: AppTheme.primaryColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            navigationController.navigateToNamed(RouteGenerator.addPlaylist);
+          },
+          child: Icon(Icons.add),
+        ));
   }
 }
